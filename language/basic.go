b2e7f7b8221bf4a8e61ed9733b208fdb1736df23
zhangliyuan01 <zhangliyuan01@baidu.com>
2016-07-05 21:06:35 +0800

    ad
    add

--- language/basic.go
+++ language/basic.go
@@ -22,6 +22,11 @@ import "fmt"
 
 const Pi = 3.14
 
+type Vertex struct {
+	x int
+	y int
+}
+
 func main() {
 	var p, q int = 2, 3
 	c := add(2, 3)
@@ -30,9 +35,32 @@ func main() {
 	a := 3;
 	b := 2;
 
+	// defer fmt.Println("world")
+	
+	// 延迟的函数调用被压入一个栈中。当函数返回时， 会按照后进先出的顺序调用被延迟的函数调用。
+	// for i := 0; i < 10; i++ {
+	// 	defer fmt.Println(i)
+	// }
+
 	a, b = swap(a, b)
 	fmt.Println(a, b, 3)
 	fmt.Println(p, q)
+	fmt.Println(Vertex{1,2})
+
+	var vnode Vertex = Vertex{4,5}
+	fmt.Println(vnode.x)
+	fmt.Println(vnode.y)
+
+	var (
+		v1 = Vertex{1, 2}  // 类型为 Vertex
+		v2 = Vertex{x: 1}  // Y:0 被省略
+		v3 = Vertex{}      // X:0 和 Y:0
+		poi  = &Vertex{1, 2} // 类型为 *Vertex
+	)
+
+	var pointer * Vertex = &vnode;
+	fmt.Println("This is pointer",pointer.x)
+	fmt.Println(v1,v2,v3,poi)
 	// fmt.Printf("Hello, world，ZLY,%g\n", math.Sqrt(16))
 }
 
